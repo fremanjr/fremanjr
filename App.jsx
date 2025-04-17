fb10b4ef60b2ec8ad50d365ad41be98c75c5bbab
Freeman Jr. <baxonfreeman250@gmail.com>
2025-04-17 09:29:15 +0200

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <h1 className="text-3xl font-bold text-center mb-6 text-blue-700">🎓 Student Management</h1>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
